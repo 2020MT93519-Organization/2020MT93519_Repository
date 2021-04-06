317a76ecbe860280bd12d86066c0af0658508fce
Dudekula Asif <2020MT93519@wilp-bits.pilani.ac.in>
2021-04-07 03:59:33 +0530

    temp to temp_1

---
+++ src/Swap_numbers.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Swap_Two_Numbers {
+
+    public static void main(String[] args) {
+
+        int value1 = 100;
+        int value2 = 200;
+        int temp;
+      
+        System.out.println("--Before swap--");
+        System.out.println("Value 1 = " + value1);
+        System.out.println("Value 2 = " + value2);
+
+        temp = value1;
+        value1 = value2;
+        value2 = temp;
+
+        System.out.println("--After swap--");
+        System.out.println("Value 1 = " + value1);
+        System.out.println("Value 2 = " + value2);
+    }
+}
